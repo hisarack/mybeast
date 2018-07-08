31a470efbb1536b3a9afc22490588ba2499ebbc7
hisarack <radiohead0401@gmail.com>
2018-07-08 16:08:30 +0800

    add hello gym holdem

---
+++ holdem/hello_gymholdem.py
@@ -0,0 +1,11 @@
+import gym
+import gymholdem
+
+env = gymholdem.TexasHoldemEnv(2)
+env.add_player(0, stack=2000)
+env.add_player(1, stack=2000)
+
+(player_states, (community_infos, community_cards)) = env.reset()
+(player_infos, player_hands) = zip(*player_states)
+
+env.render(mode='human')
