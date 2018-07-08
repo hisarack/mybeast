8354595bed53c6970bb3ad9602f76751b58fd14b
hisarack <radiohead0401@gmail.com>
2018-07-08 16:19:06 +0800

    update hello gym holdem

--- holdem/hello_gymholdem.py
+++ holdem/hello_gymholdem.py
@@ -9,3 +9,9 @@ env.add_player(1, stack=2000)
 (player_infos, player_hands) = zip(*player_states)
 
 env.render(mode='human')
+
+done = False
+while not done:
+    action = gymholdem.safe_actions(community_infos, n_seats=env.n_seats)
+    (player_states, (community_infos, community_cards)), rews, done, info = env.step(action)
+    env.render(mode='human')
