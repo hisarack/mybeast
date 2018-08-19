fb4db6c50c2138930ac4991cb9d5a36dcdfbbd48
hisarack <radiohead0401@gmail.com>
2018-08-20 00:32:17 +0800

    Update mcts bot but it is still not work, orz

--- hearts/strategy/mcts.py
+++ hearts/strategy/mcts.py
@@ -15,102 +15,128 @@ of the example by design are that moves do not commute and early mistakes are mo
 In particular there are two models of best child that one can use 
 """
 
-class State():
-    NUM_TURNS = 10	
+
+class IState(object):
+
+    def next_state(self):
+        raise NotImplementedError()
+
+    def terminal(self):
+        raise NotImplementedError()
+
+    def reward(self):
+        raise NotImplementedError()
+
+
+class State(IState):
+    NUM_TURNS = 10    
     GOAL = 0
     MOVES=[2,-2,3,-3]
     MAX_VALUE= (5.0*(NUM_TURNS-1)*NUM_TURNS)/2
     num_moves=len(MOVES)
     
     def __init__(self, value=0, moves=[], turn=NUM_TURNS):
-        self.value=value
-        self.turn=turn
-        self.moves=moves
-	
+        self.value = value
+        self.turn = turn
+        self.moves = moves
+    
     def next_state(self):
-        next_move=random.choice([x*self.turn for x  in self.MOVES])
-        next_state=State(self.value+next_move, self.moves+[next_move], self.turn-1)
+        next_move = random.choice([x*self.turn for x  in self.MOVES])
+        next_state = State(self.value+next_move, self.moves+[next_move], self.turn-1)
         return next
-	
+    
     def terminal(self):
         if self.turn == 0:
             return True
         return False
-	
+    
     def reward(self):
         r = 1.0-(abs(self.value-self.GOAL)/self.MAX_VALUE)
         return r
-	
+    
     def __hash__(self):
         return int(hashlib.md5(str(self.moves).encode('utf-8')).hexdigest(),16)
-	
+    
     def __eq__(self,other):
-        if hash(self)==hash(other):
+        if hash(self) == hash(other):
             return True
         return False
-	
+    
     def __repr__(self):
         s="Value: %d; Moves: %s"%(self.value,self.moves)
         return s
-	
+    
 
 class Node():
 
     def __init__(self, state, parent=None):
         self.visits=1
-        self.reward=0.0	
+        self.reward=0.0    
         self.state=state
         self.children=[]
-        self.parent=parent	
+        self.parent=parent    
     
     def add_child(self, child_state):
-        child=Node(child_state,self)
+        for child in self.children:
+            if child.state == child_state:
+                return
+        child=Node(child_state, self)
         self.children.append(child)
-	
-    def update(self,reward):
+ 
+    def move_to_child(self, child_state):
+        self.add_child(child_state)
+        for child in self.children:
+            if child_state == child_state:
+                return child
+        print('can not find child')
+        return None
+
+    def update(self, reward):
         self.reward+=reward
         self.visits+=1
-	
+    
     def fully_expanded(self):
-        if len(self.children)==self.state.num_moves:
+        if len(self.children) == self.state.num_moves:
             return True
         return False
-	
+    
     def __repr__(self):
         s="Node; children: %d; visits: %d; reward: %f"%(len(self.children),self.visits,self.reward)
         return s
-		
+        
 
 class MCTS(object):
-    #MCTS scalar.  Larger scalar will increase exploitation, smaller will increase exploration. 
-    SCALAR=1/math.sqrt(2.0)
+    
+    def __init__(self, budget):
+        self_budget = budget
+        self.SCALAR = 1 / math.srt(2.0) # larger scalar will increase exploitation, smaller will increase exploration
 
-    def UCTSEARCH(self, budget,root):
-        for iter in range(int(budget)):
-            front=self.TREEPOLICY(root)
-            reward=self.DEFAULTPOLICY(front.state)
-            self.BACKUP(front,reward)
-        return self.BESTCHILD(root,0)
+    def UCTSEARCH(self, root):
+        for iter in range(int(self._budget)):
+            front = self.TREEPOLICY(root)
+            reward = self.DEFAULTPOLICY(front.state)
+            self.BACKUP(front, reward)
+        return self.BESTCHILD(root, 0)
 
     def TREEPOLICY(self, node):
         #a hack to force 'exploitation' in a game where there are many options, and you may never/not want to fully expand first
-        while node.state.terminal()==False:
-            if len(node.children)==0:
+        while node.state.terminal() is False:
+            if len(node.children) == 0:
                 return self.EXPAND(node)
-            elif random.uniform(0,1)<.5:
-                node=self.BESTCHILD(node,SCALAR)
-            else:
-                if node.fully_expanded()==False:	
-                    return self.EXPAND(node)
-                else:
-                    node=self.BESTCHILD(node,SCALAR)
+            elif random.uniform(0,1) < .5: # exploitation
+                node=self.BESTCHILD(node, SCALAR)
+            elif node.fully_expanded() is False: # exploration   
+                return self.EXPAND(node)
+            else:  
+                node=self.BESTCHILD(node, SCALAR)
         return node
 
     def EXPAND(self, node):
+        # find out the un-expanded state
         tried_children=[c.state for c in node.children]
-        new_state=node.state.next_state()
+        new_state = node.state.next_state()
         while new_state in tried_children:
-            new_state=node.state.next_state()
+            new_state = node.state.next_state()
         node.add_child(new_state)
         return node.children[-1]
 
@@ -119,25 +145,25 @@ class MCTS(object):
         bestscore=0.0
         bestchildren=[]
         for c in node.children:
-            exploit=c.reward/c.visits
-            explore=math.sqrt(2.0*math.log(node.visits)/float(c.visits))	
-            score=exploit+scalar*explore
-            if score==bestscore:
+            exploit = c.reward / c.visits
+            explore = math.sqrt(2.0*math.log(node.visits) / float(c.visits))    
+            score = exploit+scalar*explore
+            if score == bestscore:
                 bestchildren.append(c)
-            if score>bestscore:
-                bestchildren=[c]
-                bestscore=score
-        if len(bestchildren)==0:
+            if score > bestscore:
+                bestchildren = [c]
+                bestscore = score
+        if len(bestchildren) == 0:
             print("OOPS: no best child found, probably fatal")
         return random.choice(bestchildren)
 
     def DEFAULTPOLICY(self, state):
-        while state.terminal()==False:
-            state=state.next_state()
+        while state.terminal() is False:
+            state = state.next_state()
         return state.reward()
 
     def BACKUP(self, node, reward):
-        while node!=None:
-            node.visits+=1
-            node.reward+=reward
-            node=node.parent
+        while node is not None:
+            node.visits += 1
+            node.reward += reward
+            node = node.parent
--- hearts/strategy/mctsplay.py
+++ hearts/strategy/mctsplay.py
@@ -1,3 +1,6 @@
+import random
+import hashlib
+
 import gym
 from gymhearts import env as hearts_env
 from gymhearts import strategy
@@ -8,17 +11,95 @@ from treys import Card
 
 from .completeplay import CompletePlayStrategy
 from .mcts import MCTS
+from .mcts import Node
+from .mcts import IState
+
+
+class HeartState(IState):
+    NUM_TURNS = 13   
+    GOAL = 0
+    MOVES=[2,-2,3,-3]
+    MAX_VALUE= (5.0*(NUM_TURNS-1)*NUM_TURNS)/2
+    num_moves=len(MOVES)
+
+    def __init__(self, observation, info):
+        deck = Deck()
+        self._available_cards = []
+        self._score = score
+        self._trick = trick
+        self._playing_cards = playing_cards
+        self._player_id = player_id
+        self._evaluator = Evaluator()
+        self._done = done
+
+    def next_state(self):
+        trick = observation['trick']
+        player_id = observation['current_player_id']
+        number_of_players = observation['number_of_players']
+        playing_cards = random.shuffle(info['competitor_cards'], number_of_players-1)
+        playing_cards.insert(player_id, random.choice(observation['valid_hand_cards']))
+        looser_score, looser_player_id = self._evaluator.evalute(playing_cards, playing_ids)
+        next_score = self._score
+        if looser_player_id != 0:
+            next_score += looser_score
+        done = False
+        if len(info['competitor_cards']) == number_of_players-1:
+            done = True
+        return State(
+            score = next_score,
+            trick = self._trick + 1,
+            playing_cards = playing_cards,
+            player_id = player_id,
+            done = done
+        )
+    
+    def get_action_card(self):
+        return self._playing_cards[self._player_id]
+
+    def terminal(self):
+        return self._done
+    
+    def reward(self):
+        r = 1.0 - self._score / 26
+        return r
+    
+    def __hash__(self):
+        return int(hashlib.md5(
+            (self._trick + ":" + str(self._played_cards)).encode('utf-8')
+        ).hexdigest(), 16)
+    
+    def __eq__(self,other):
+        if hash(self) == hash(other):
+            return True
+        return False
+    
+    def __repr__(self):
+        return "Score: %d; Playing Cards: %s "% (self._score, self._played_cards)
 
 
 class MCTSPlayStrategy(strategy.IStrategy):
     
-    def __init__(self):
+    def __init__(self, budget):
         self._evaluator = Evaluator()
         deck = Deck()
         self._available_cards = deck.draw(52)
+        self._mcts = MCTS(budget)
+        self._current_node = Node(HeartState(
+            score = 0,
+            trick = 0,
+            playing_cards = [],
+            done = False
+        ))
 
     def move(self, observation):
-        pass 
+        competitor_cards = self._available_cards.copy()
+        hand_cards = observation['hand_cards']
+        for c in hand_cards:
+            competitor_cards.remove(c)
+        info = {}
+        info['competitor_cards'] = competitor_cards
+        best_next_node = self._mcts.UCTSEARCH(self._current_node)
+        return best_next_node.state.get_action_card()
 
     def watch(self, observation, info):
         if info['done'] is True:
@@ -29,5 +110,12 @@ class MCTSPlayStrategy(strategy.IStrategy):
         else:
             played_card = info['action']
             self._available_cards.remove(played_card)
-        
+        playing_cards = observation['playing_cards']
+        if len(playing_cards) == 4:
+            self._current_node.move_to_child(HeartState(
+                score = score,
+                trick = observation['trick'],
+                playing_cards = played_cards,
+                done = info['done']
+            ))
 
